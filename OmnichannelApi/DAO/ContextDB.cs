97ac67cff036f95f73abf27cbaa894881058a4cc
Victor <victorguedes.js@gmail.com>
2025-03-21 23:14:27 -0300

    Adicionar arquivos de projeto.

---
+++ OmnichannelApi/DAO/ContextDB.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OmnichannelApi.Model;
+
+namespace OmnichannelApi.DAO
+{
+    public class ContextDB : DbContext
+    {
+        
+       public DbSet<User> Users { get; set; }
+
+
+        public ContextDB(DbContextOptions<ContextDB> opcoes) : base(opcoes)
+        {
+
+
+        }
+    }
+}
---
+++ OmnichannelApi/Model/User.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OmnichannelApi.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        public string? PasswordHash { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Cep { get; set; }
+
+        [Required]
+        public PreferredChannel PreferredChannel { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsActive { get; set; } = true;
+
+    }
+
+    public enum PreferredChannel
+    {
+        Email,
+        SMS,
+        WhatsApp,
+        ChatOnline,
+        SocialMedia
+    }
+}
